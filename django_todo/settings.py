696e4de41a0c21e55c6bb2fa3682ade351e14ebd
Ca-ananay <caananay.maduka@gmail.com>
2017-08-05 21:49:55 +0100

    added static paths

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -135,9 +135,13 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 #
-# STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
