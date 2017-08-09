e346cf31a647a04232bd4859d3e9acd5703f8025
Ca-ananay <caananay.maduka@gmail.com>
2017-08-09 17:57:49 +0100

    updated settings

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -106,11 +106,11 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-    # 'default': dj_database_url.config('CLEARDB_DATABASE_URL')
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config('CLEARDB_DATABASE_URL')
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
 }
 
 
